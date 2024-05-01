7b03ca370786ba4ff4358f100e08f3a2f59418e4
Goulven Clec'h <goulven.clech@protonmail.com>
2024-05-01 18:26:06 +0200

    fix: code and spacing styles

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,15 @@ import astropi from "@goulvenclech/astropi"
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      themes: {
+        // TODO: should be changed to "one-light" when Astro will support it
+        light: "one-dark-pro",
+        dark: "one-dark-pro",
+      },
+    },
+  },
   integrations: [
     astropi({
       projectName: "Astropi",
